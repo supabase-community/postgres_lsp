b51b52fd725e939939d07b12fafeba8649b98335
psteinroe <philipp@steinroetter.com>
2024-03-14 14:20:32 +0100

    feat: basic base db

--- crates/base_db/src/statement.rs
+++ crates/base_db/src/statement.rs
@@ -1,1 +1,24 @@
-pub struct Document {}
+pub struct Statement {
+    pub version: i32,
+
+    parser: tree_sitter::Parser,
+
+    pub tree: tree_sitter::Tree,
+}
+
+impl Statement {
+    pub fn parse(content: String) -> Self {
+        let mut parser = tree_sitter::Parser::new();
+        parser
+            .set_language(tree_sitter_sql::language())
+            .expect("Error loading sql language");
+
+        let tree = parser.parse(&content, None).unwrap();
+
+        Self {
+            version: 0,
+            parser,
+            tree,
+        }
+    }
+}
