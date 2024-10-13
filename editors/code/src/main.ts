b59c83cc4aff30e9355914d3bbceac675c653b29
Julian <juliandomke@outlook.de>
2024-10-13 10:41:42 +0200

    fix: extension command, server crate path

--- editors/code/src/main.ts
+++ editors/code/src/main.ts
@@ -1,46 +1,41 @@
-import { ExtensionContext } from 'vscode';
+import { ExtensionContext } from "vscode";
 
 import {
-    Executable,
-    LanguageClient,
-    LanguageClientOptions,
-    ServerOptions,
-} from 'vscode-languageclient/node';
+  Executable,
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+} from "vscode-languageclient/node";
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext) {
-    // If the extension is launched in debug mode then the debug server options are used
-    // Otherwise the run options are used
-    const run: Executable = {
-        command: 'postgres_lsp',
-    };
-    const serverOptions: ServerOptions = {
-        run,
-        debug: run
-    };
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the run options are used
+  const run: Executable = {
+    command: "pglsp",
+  };
+  const serverOptions: ServerOptions = {
+    run,
+    debug: run,
+  };
 
-    // Options to control the language client
-    const clientOptions: LanguageClientOptions = {
-        // Register the server for plain text documents
-        documentSelector: [{ scheme: 'file', language: 'sql' }]
-    };
+  // Options to control the language client
+  const clientOptions: LanguageClientOptions = {
+    // Register the server for plain text documents
+    documentSelector: [{ scheme: "file", language: "sql" }],
+  };
 
-    // Create the language client and start the client.
-    client = new LanguageClient(
-        'postgres_lsp',
-        'Postgres LSP',
-        serverOptions,
-        clientOptions
-    );
+  // Create the language client and start the client.
+  client = new LanguageClient("postgres_lsp", "Postgres LSP", serverOptions, clientOptions);
 
-    // Start the client. This will also launch the server
-    client.start();
+  // Start the client. This will also launch the server
+  client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-    if (!client) {
-        return undefined;
-    }
-    return client.stop();
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
 }
