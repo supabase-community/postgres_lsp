5d9416935c344d574f6fbc9b505cd91a54e9b354
Julian <juliandomke@outlook.de>
2024-10-13 10:49:30 +0200

    use previous prettier config

--- editors/code/.prettierrc.js
+++ editors/code/.prettierrc.js
@@ -1,5 +1,7 @@
 module.exports = {
-    // use 100 because it's Rustfmt's default
-    // https://rust-lang.github.io/rustfmt/?version=v1.4.38&search=#max_width
-    printWidth: 100,
+  // use 100 because it's Rustfmt's default
+  // https://rust-lang.github.io/rustfmt/?version=v1.4.38&search=#max_width
+  printWidth: 100,
+  singleQuote: true,
+  tabWidth: 4,
 };
