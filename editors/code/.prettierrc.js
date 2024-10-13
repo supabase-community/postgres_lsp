fedfd9ace07866dc18ca832b2e78a88e2276641a
Julian <juliandomke@outlook.de>
2024-10-13 11:01:28 +0200

    more prettier

--- editors/code/.prettierrc.js
+++ editors/code/.prettierrc.js
@@ -1,7 +1,8 @@
 module.exports = {
-  // use 100 because it's Rustfmt's default
-  // https://rust-lang.github.io/rustfmt/?version=v1.4.38&search=#max_width
-  printWidth: 100,
-  singleQuote: true,
-  tabWidth: 4,
+    // use 100 because it's Rustfmt's default
+    // https://rust-lang.github.io/rustfmt/?version=v1.4.38&search=#max_width
+    printWidth: 100,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'none'
 };
--- editors/code/src/main.ts
+++ editors/code/src/main.ts
@@ -4,7 +4,7 @@ import {
     Executable,
     LanguageClient,
     LanguageClientOptions,
-    ServerOptions,
+    ServerOptions
 } from 'vscode-languageclient/node';
 
 let client: LanguageClient;
@@ -13,21 +13,28 @@ export function activate(context: ExtensionContext) {
     // If the extension is launched in debug mode then the debug server options are used
     // Otherwise the run options are used
     const run: Executable = {
-        command: 'pglsp',
+        command: 'pglsp'
     };
     const serverOptions: ServerOptions = {
         run,
-        debug: run,
+        debug: run
     };
 
     // Options to control the language client
     const clientOptions: LanguageClientOptions = {
         // Register the server for plain text documents
-        documentSelector: [{ scheme: 'file', language: 'sql' }],
+        documentSelector: [
+            { scheme: 'file', language: 'sql' }
+        ]
     };
 
     // Create the language client and start the client.
-    client = new LanguageClient('postgres_lsp', 'Postgres LSP', serverOptions, clientOptions);
+    client = new LanguageClient(
+        'postgres_lsp',
+        'Postgres LSP',
+        serverOptions,
+        clientOptions
+    );
 
     // Start the client. This will also launch the server
     client.start();
