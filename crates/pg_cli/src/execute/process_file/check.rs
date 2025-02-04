96901d9d1ead590621ab85340b17a12e6fdd8b78
Julian <juliandomke@outlook.de>
2025-02-04 09:16:05 +0100

    hm

--- crates/pg_cli/src/execute/process_file/check.rs
+++ crates/pg_cli/src/execute/process_file/check.rs
@@ -35,7 +35,6 @@ pub(crate) fn check_with_guard<'ctx>(
                     max_diagnostics,
                     only,
                     skip,
-                    true,
                 )
                 .with_file_path_and_code(
                     workspace_file.path.display().to_string(),
