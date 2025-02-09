2068d971b8303877215892ff663b78bc604ab15d
Julian <juliandomke@outlook.de>
2025-02-09 09:46:06 +0100

    feat(analyser): banDropNotNull rule

--- crates/pg_analyser/src/lint/safety.rs
+++ crates/pg_analyser/src/lint/safety.rs
@@ -2,4 +2,5 @@
 
 use pg_analyse::declare_lint_group;
 pub mod ban_drop_column;
-declare_lint_group! { pub Safety { name : "safety" , rules : [self :: ban_drop_column :: BanDropColumn ,] } }
+pub mod ban_drop_not_null;
+declare_lint_group! { pub Safety { name : "safety" , rules : [self :: ban_drop_column :: BanDropColumn , self :: ban_drop_not_null :: BanDropNotNull ,] } }
--- crates/pg_analyser/src/options.rs
+++ crates/pg_analyser/src/options.rs
@@ -3,3 +3,5 @@
 use crate::lint;
 pub type BanDropColumn =
     <lint::safety::ban_drop_column::BanDropColumn as pg_analyse::Rule>::Options;
+pub type BanDropNotNull =
+    <lint::safety::ban_drop_not_null::BanDropNotNull as pg_analyse::Rule>::Options;
