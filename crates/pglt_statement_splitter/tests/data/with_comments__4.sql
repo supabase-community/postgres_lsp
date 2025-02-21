f2a8e37e5cceaddc7c52c47b4a2220abf4203bc8
psteinroe <philipp@steinroetter.com>
2025-02-21 16:50:21 +0100

    chore(pglt_statement_splitter): add test for comments

---
+++ crates/pglt_statement_splitter/tests/data/with_comments__4.sql
@@ -0,0 +1,13 @@
+-- test
+select id, name, test1231234123, unknown from co;
+
+-- in between two statements
+
+select 14433313331333 -- after a statement
+
+alter table --within a statement
+test drop column id;
+
+select lower('test');
+--after a statement
+
