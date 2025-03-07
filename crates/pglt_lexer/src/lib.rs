9496a0487d1fd4a3ed7742a0425641e52bb38a4a
psteinroe <philipp@steinroetter.com>
2025-03-07 13:36:47 +0100

    debug

--- crates/pglt_lexer/src/lib.rs
+++ crates/pglt_lexer/src/lib.rs
@@ -65,12 +65,12 @@ static PATTERN_LEXER: LazyLock<Regex> = LazyLock::new(|| {
     #[cfg(windows)]
     {
         // On Windows, treat \r\n as a single newline token
-        Regex::new(r"(?P<whitespace> +)|(?P<newline>\r\n|\n+)|(?P<tab>\t+)").unwrap()
+        Regex::new(r"(?P<whitespace> +)|(?P<newline>\r\n|\n)|(?P<tab>\t)").unwrap()
     }
     #[cfg(not(windows))]
     {
         // On other platforms, just check for \n
-        Regex::new(r"(?P<whitespace> +)|(?P<newline>\n+)|(?P<tab>\t+)").unwrap()
+        Regex::new(r"(?P<whitespace> +)|(?P<newline>\n)|(?P<tab>\t)").unwrap()
     }
 });
 
@@ -78,6 +78,7 @@ fn whitespace_tokens(input: &str) -> VecDeque<Token> {
     let mut tokens = VecDeque::new();
 
     for cap in PATTERN_LEXER.captures_iter(input) {
+        println!("{:?}", cap);
         if let Some(whitespace) = cap.name("whitespace") {
             tokens.push_back(Token {
                 token_type: TokenType::Whitespace,
