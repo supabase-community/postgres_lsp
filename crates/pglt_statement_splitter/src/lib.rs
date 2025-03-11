93052b38aca15474707c1d0352e5985c6fac6e55
Philipp Steinrötter <philipp@steinroetter.com>
2025-03-11 13:44:27 +0100

    fix: properly split triggers with or (#228)

--- crates/pglt_statement_splitter/src/lib.rs
+++ crates/pglt_statement_splitter/src/lib.rs
@@ -154,13 +154,13 @@ mod tests {
 
 create trigger default_key before insert on appointment_type for each row when (new.key is null) execute procedure default_key ();
 
-create trigger default_key before insert on appointment_status for each row when (new.key is null) execute procedure default_key ();
+create trigger default_key before insert or update on appointment_status for each row when (new.key is null) execute procedure default_key ();
 
 alter table deal_type add column key text not null;
 ")
             .expect_statements(vec!["alter table appointment_status add constraint valid_key check (private.strip_special_chars(key) = key and length(key) > 0 and length(key) < 60);",
                 "create trigger default_key before insert on appointment_type for each row when (new.key is null) execute procedure default_key ();",
-                "create trigger default_key before insert on appointment_status for each row when (new.key is null) execute procedure default_key ();",
+                "create trigger default_key before insert or update on appointment_status for each row when (new.key is null) execute procedure default_key ();",
                 "alter table deal_type add column key text not null;",
             ]);
     }
