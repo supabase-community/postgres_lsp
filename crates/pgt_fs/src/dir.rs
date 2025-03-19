f054a84d8bcc3a4e5e54613f76b757ba1f368e9f
psteinroe <philipp@steinroetter.com>
2025-03-19 18:23:14 +0100

    fix: dir comment

--- crates/pgt_fs/src/dir.rs
+++ crates/pgt_fs/src/dir.rs
@@ -4,8 +4,8 @@ use tracing::warn;
 
 pub fn ensure_cache_dir() -> PathBuf {
     if let Some(proj_dirs) = ProjectDirs::from("dev", "supabase-community", "pgt") {
-        // Linux: /home/alice/.cache/pglt
-        // Win: C:\Users\Alice\AppData\Local\supabase-community\pglt\cache
+        // Linux: /home/alice/.cache/pgt
+        // Win: C:\Users\Alice\AppData\Local\supabase-community\pgt\cache
         // Mac: /Users/Alice/Library/Caches/dev.supabase-community.pgt
         let cache_dir = proj_dirs.cache_dir().to_path_buf();
         if let Err(err) = fs::create_dir_all(&cache_dir) {
