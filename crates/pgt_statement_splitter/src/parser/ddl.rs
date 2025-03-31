d3ad18ca4f3b5c1900f0fb26d31d97422a7af152
psteinroe <philipp@steinroetter.com>
2025-03-31 18:11:32 +0200

    fix: view with options

--- crates/pgt_statement_splitter/src/parser/ddl.rs
+++ crates/pgt_statement_splitter/src/parser/ddl.rs
@@ -5,7 +5,7 @@ use super::{Parser, common::unknown};
 pub(crate) fn create(p: &mut Parser) {
     p.expect(SyntaxKind::Create);
 
-    unknown(p, &[]);
+    unknown(p, &[SyntaxKind::With]);
 }
 
 pub(crate) fn alter(p: &mut Parser) {
